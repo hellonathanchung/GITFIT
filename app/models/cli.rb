8aedc826166115d531b637cf370b86c219a090a6
hellonathanchung <chung.nathanh@gmail.com>
2020-07-29 16:03:55 -0700

    building out some cli apps

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -9,7 +9,7 @@ prompt = TTY::Prompt.new
         
         puts "Do you have an account?"
         
-        user_input = gets.chomp.downcase #as king for user input
+        user_input = gets.chomp.downcase 
 
         if user_input == 'yes'
             puts "What is your username?"
@@ -17,7 +17,7 @@ prompt = TTY::Prompt.new
 
             if User.exists?(:name => username)
                 $user = User.find_by(name: username)
-                puts ("Welcome back, #{$user.name}!").titleize #titleize capitalizes the first letter of a string
+                puts ("Welcome, #{$user.name}!").titleize #titleize capitalizes the first letter of a string
             else 
                 puts "We could not find your username"
                 add_new_user
@@ -25,101 +25,130 @@ prompt = TTY::Prompt.new
         else
             add_new_user
         end
-    end
 
-    def add_new_user
-        puts "Please enter your username"
-        username = gets.chomp
-        if User.exists?(:name => username)
-            puts "Username is taken, please try again"
-            add_new_user
-        else 
-            new_user = User.create(:name => username)
-            $user = new_user
-            puts "Hello, #{$user.name}!"
-
-            puts "What is your fitness level?".titleize
-            puts "Active"
-            puts "Moderate"
-            puts "Sedentary"
-            fitness_level = gets.chomp.downcase
-            username.fitness_level = fitness_level
-            puts "Your fitness level is #{fitness_level}"
-            
-        end
-    end
-
-    def read_exercises #workouts
-        puts "want to read through the exercises?"
-        input = gets.chomp.downcase
-
-        if input == 'yes'
-            put "What exercise would you like to learn about?"
-            exercise = gets.chomp.downcase
-
-            Exercise.all.map {|exercise| "#{exercise.name} -  #{exercise.description}"}
-        else 
-            #something
-        end
-    end
+        def add_new_user
+            puts "Please create your username"
+            username = gets.chomp
+            if User.exists?(name: username)
+                puts "Username is taken, please try again"
+                add_new_user
+            else 
+                new_user = User.create(name: username)
+                $user = new_user
+                
+                puts "Hello, #{$user.name.titleize}!"
     
-    def find_all_workouts
+                puts "What is your fitness level?".titleize
+                puts "Active"
+                puts "Moderate"
+                puts "Sedentary"
+                fitness_level = gets.chomp.downcase
+                $user.fitness_level = fitness_level
+                puts "Your fitness level is #{fitness_level}"
+                
+            end
+        end
     end
 
     def menu 
-        require "tty-prompt"
+            require "tty-prompt"
+            
+            prompt = TTY::Prompt.new
+            # new_session = "New Session"
+            # review_previous_session = "Review Previous Sessions"
+            # see_all_exercises = "See All Exercises"
+            # view_last_session = "View Last Session"
+            # choices = %w(new_session review_previous_session see_all_exercises view_last_session)
+            prompt.select("What would you like to do?") do |menu|
+                menu.enum "."
+            
+                menu.choice "New Session",-> {new_session} 
+                menu.choice "Review Previous Sessions",->{review_previous_sessions}
+                menu.choice "View Last Session",->{view_last_session}
+                menu.choice "Learn About Exercises",
+            
+            
+                def read_exercises
+                    exercises = Exercise.all 
+                    exercises.map {|exercise| "#{exercise.name} -  #{exercise.description}"}
+                end
 
-        prompt = TTY::Prompt.new
-    # new_session = "New Session"
-    # review_previous_session = "Review Previous Sessions"
-    # see_all_exercises = "See All Exercises"
-    # view_last_session = "View Last Session"
-    # choices = %w(new_session review_previous_session see_all_exercises view_last_session)
-        prompt.select("What would you like to do?") do |menu|
-            menu.enum "."
+                def select_exercise
+                    Prompt = TTY::Prompt.new
+                    puts "Starting a new session..."
+                    prompt.select("What exercise would you like to do?") do |menu|
+                        menu.enum "."
+                    
+                        menu.choice "push ups",-> {new_session} 
+                        menu.choice "pull ups",->{review_previous_sessions}
+                        menu.choice "squats",->
+                        menu.choice "leg raises",
+                        menu.choice "crunches",->
+                        menu.choice "jumping jacks",
+                            puts "great, youve picked out your exercise"
+                            puts "how many reps would you like to do?"
+                                reps = gets.chomp
+                                #how many sets would you like to do => sets
+                                sets = gets.chomp
+                                Workout.create(self, exercise_id, sets, reps )
+                        end
+                end
+
+                def new_session
+                    puts "Starting a new session..."
+                    select_exercise
+                    puts "Awesome. Would you like to do another exercise?"
+                    input == gets.chomp.downcase
+                        if input == yes
+                            select_exercise
+                        else
+                            puts "Goodbye!"
+                            menu
+                        end
+                end
+            end
         
-            menu.choice "New Session",-> {new_session} 
-            menu.choice "Review Previous Sessions",->{review_previous_sessions}
-            menu.choice "View Last Session",->{view_last_session}
-            menu.choice "Learn About Exercises",->{read_exercises}
+        # def read_exercises #workouts
+        #     puts "want to read through the exercises?"
+        #     input = gets.chomp.downcase
+        
+        #     if input == 'yes'        
+        #         User.first.exercises.all.map {|exercise| "#{exercise.name} -  #{exercise.description}"}
+        #     end
+        
+        def find_all_workout
+                puts "Would you like to see all of your past sessions?(yes/no)"
+                user_input = gets.chomp.downcase 
+                # binding.pry
+                if user_input == 'yes'
+                    $user = User.find_by(name: "Brian")
+                    $user.workouts.each do |workout| 
+                        puts workout.exercise.name
+                        puts workout.exercise.description
+                        puts workout.exercise.body_part
+                        puts "#{workout.sets}  sets"
+                        puts "#{workout.reps}  reps" 
+                    end
+                end
+        end
+        
+        def review_previous_session
+            # binding.pry
+            puts "Here are your previous sessions"
+            #select workouts from the day the user did it. 
+
+            #What would you like to do? Prompt 
+            #edit session
+            #exit -> menu
+            #update a session
         end
 
-        def read_exercises #workouts
-            binding.pry
-            # puts "want to read through the exercises?"
-            # input = gets.chomp.downcase
 
-            # if input == 'yes'
-            #     put "What exercise would you like to learn about?"
-            #     exercise = gets.chomp.downcase
-
-                exercises = Exercise.all.map {|exercise| "#{exercise.name} -  #{exercise.description}"}
-                exercises
-            # else 
-                #something
-            # end
-        end
-    end
-
-    def review_previous_session
-        binding.pry
-        puts "Here are your previous sessions"
-        session
-        self.workouts.
         def edit_session
-        end
+            #what would you like to update?
+            #sets? (user.workouts.update(sets: value)
+            #reps? (user.workouts.update(sets: value))
+        end 
     end
-
-    def new_session
-    end
-
-    def edit_sets #this would be something like user.workouts.first.repss = value
-        puts "current sets: #{reps}"
-        puts "How many sets did you want to do?"
-    end
-
-    def edit_sets #this would be something like self.workouts.first or select a workout.sets = value
-        puts "current sets: #{sets}"
-        puts "How many sets did you want to do?"
-    # end
+end
 end
--- bin/run.rb
+++ bin/run.rb
@@ -3,8 +3,6 @@ require 'tty-prompt'
 
 
 cli = CommandLine.new
-<<<<<<< HEAD
+
 cli.welcome
-=======
->>>>>>> c4d710ae3a85c7cd531055043ef900d858430c3e
 cli.menu
