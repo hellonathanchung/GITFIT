b08fa25bd7b2522ea60daace518f53f194212f74
hellonathanchung <chung.nathanh@gmail.com>
2020-07-30 11:27:29 -0700

    trying-to-restructure-a-session

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -27,7 +27,7 @@ class CLI #class for CLI
                 fitness_level = ask_fitness_level #gets fitness level
                 $user =User.create(name:username, age: age, fitness_level: fitness_level)
                 puts "Account sucessfully created. Welcome to GitFIT #{username}."
-                binding.pry
+                # binding.pry
                 
             end
     end
@@ -51,9 +51,10 @@ class CLI #class for CLI
                         
                 menu.choice "New Session",-> {new_session} 
                 menu.choice "Review Previous Sessions",->{review_previous_sessions}
-                menu.choice "View Last Session",->{view_last_session}
-                menu.choice "Learn About Exercises",-> {Exercise.all.map {|exercise| "#{exercise.name} -  #{exercise.description}"}}
-                end
+                menu.choice "Edit a session",->{edit_session}
+                menu.choice "Learn About Exercises",-> {read_exercises}
+                menu.choice "Exit",-> {exit}
+                end       
     end
 #######################################################
     def ask_age #done
@@ -75,7 +76,8 @@ class CLI #class for CLI
     end
     
     def new_session
-        new_session = Session.create()
+        system "clear"
+        make_a_session = Session.create(user_id: $user.id)
         prompt = TTY::Prompt.new
         puts ("
 ██╗     ███████╗████████╗███████╗     ██████╗ ███████╗████████╗    ███████╗████████╗ █████╗ ██████╗ ████████╗███████╗██████╗ 
@@ -84,13 +86,79 @@ class CLI #class for CLI
 ██║     ██╔══╝     ██║   ╚════██║    ██║   ██║██╔══╝     ██║       ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██╔══╝  ██║  ██║
 ███████╗███████╗   ██║   ███████║    ╚██████╔╝███████╗   ██║       ███████║   ██║   ██║  ██║██║  ██║   ██║   ███████╗██████╔╝
 ╚══════╝╚══════╝   ╚═╝   ╚══════╝     ╚═════╝ ╚══════╝   ╚═╝       ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═════╝ 
-                                                                                                                             
 ")
         sleep 2
         system "clear"
         puts "Let's select a workout"
+# binding.pry
+        new_workout
+        menu
     end
-    def read_exercises
-        Exercise.all.map {|exercise| "#{exercise.name} -  #{exercise.description}"}
+
+    def new_workout
+        input = nil
+        until input == "no" do
+            prompt = TTY::Prompt.new
+            exercise_choice = prompt.select("What exercise would you like to do?") do |menu|
+                menu.enum "."
+                                    
+                menu.choice "push-ups", value: 1 
+                menu.choice "pull-ups", value: 2
+                menu.choice "squats", value: 3
+                menu.choice "leg raises", value: 4
+                menu.choice "crunches", value: 5 
+                menu.choice "jumping jacks", value: 6
+            end
+            number_of_reps = prompt.ask("How many reps did you do?")
+            number_of_sets = prompt.ask("How many sets did you do?")            
+            create_workout = Workout.create(session_id: $user.sessions.last.id, exercise_id:exercise_choice[:value], sets:number_of_sets, reps:number_of_reps)
+            puts "Would you like to add another?"
+            input = gets.chomp.downcase
+        end
+
+        puts "Would you like to name this section?"
+        Session.last
+        
+        system "clear"
+        puts "Sending you back to main menu"
+        sleep 3
+
+        # new_workout = Exercise.create()
+    end
+
+    def read_exercises 
+        input = nil
+        prompt = TTY::Prompt.new
+        exercise_choice = prompt.select("What exercise would you like to read about?") do |menu|
+            menu.enum "."
+                                
+            menu.choice "push-ups", value: 1 
+            menu.choice "pull-ups", value: 2
+            menu.choice "squats", value: 3
+            menu.choice "leg raises", value: 4
+            menu.choice "jumping jacks", value: 5
+            menu.choice "crunches", value: 5
+        end
+        
+        selected_exercise = Exercise.find_by({id:exercise_choice[:value]})
+        # binding.pry
+            puts ("Exercise |#{selected_exercise.name}
+             Body Part | #{selected_exercise.body_part}
+             Description | #{selected_exercise.description}")
+
+        puts "Would you like to read about another exercise?"
+  
+        input = gets.chomp.downcase
+        if input == "yes"
+            read_exercisese
+        else 
+            menu
+        end
+    end
+
+    def review_previous_sessions
+        system "clear"
+        session = Session.all.find_by($user.id)
+        session.user.sessions.last
     end
 end
